362e7bc4b7bcb088e53c6983a0fbe3b9643403dc
LainePereira <144857991+LainePereira@users.noreply.github.com>
2023-11-20 21:12:39 -0300

    Create carminaburana.c
    o presente exercicio, visa retirar as vogais do nome Carmina Burana. Por se tratar de um exercísio executado por principiante, pode conter erros.

---
+++ carminaburana.c
@@ -0,0 +1,18 @@
+#include "cs50.h"
+#include "stdio.h"
+
+int main(void) {
+    char_nome = "Carmina Burana";
+
+    int extenção = (nome);
+
+    printf("Vogais '%s', nome);
+
+    for (int i = 0; i < extenção; i++) {
+        if (nome[i] = ('a' 'i' 'u') nome[i] = 'A' 'I' 'U' ) {
+            printf("%c ", nome[i]);
+        
+    }
+
+    printf("\n");
+}
