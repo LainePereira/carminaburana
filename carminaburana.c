505727f56538cf5da25f9611e8cfc95cfdf463ce
LainePereira <144857991+LainePereira@users.noreply.github.com>
2023-11-21 11:59:30 -0300

    Update carminaburana.c
    correção de código, atividade com objetivo de aprendizado

--- carminaburana.c
+++ carminaburana.c
@@ -1,18 +1,21 @@
-#include "cs50.h"
-#include "stdio.h"
+#include <stdio.h>
+#include <string.h>
 
 int main(void) {
-    char_nome = "Carmina Burana";
+    char nome[] = "Carmina Burana";
 
-    int extenção = (nome);
+    int extensao = strlen(nome);
 
-    printf("Vogais '%s', nome);
+    printf("Vogais de '%s': ", nome);
 
-    for (int i = 0; i < extenção; i++) {
-        if (nome[i] = ('a' 'i' 'u') nome[i] = 'A' 'I' 'U' ) {
+    for (int i = 0; i < extensao; i++) {
+        if (nome[i] == 'a' || nome[i] == 'i' || nome[i] == 'u' ||
+            nome[i] == 'A' || nome[i] == 'I' || nome[i] == 'U') {
             printf("%c ", nome[i]);
-        
+        }
     }
 
     printf("\n");
+
+    return 0;
 }
